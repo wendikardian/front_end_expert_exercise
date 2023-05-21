1d9813c4131966e0980d43181efc3a0d28cd3d77
Wendi Kardian <69452468+wendikardian@users.noreply.github.com>
2023-05-21 22:30:13 +0700

    update

---
+++ ESLintSample/src/sample.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-console */
+const ENGLISH = 'english';
+const INDONESIA = 'indonesia';
+const JAPAN = 'japan';
+
+const sayHello = (language) => {
+  switch (language) {
+    case ENGLISH:
+      return 'Hello! How are you?';
+    case INDONESIA:
+      return 'Halo! Apa kabar?';
+    case JAPAN:
+      return 'Konnichiwa! Ogenkidesuka?';
+    default:
+      return 'Hello!';
+  }
+};
+
+console.log(sayHello(INDONESIA));
