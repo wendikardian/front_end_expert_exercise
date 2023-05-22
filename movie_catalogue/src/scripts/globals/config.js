6c8eb52e936f8925262d64cf551c8da1dd92e83b
Wendi Kardian <69452468+wendikardian@users.noreply.github.com>
2023-05-22 11:49:05 +0700

    updated with render some data

---
+++ movie_catalogue/src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: '282a1065ab1d4d2195d05412746e523a',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
--- movie_catalogue/src/scripts/views/pages/now-playing.js
+++ movie_catalogue/src/scripts/views/pages/now-playing.js
@@ -1,11 +1,23 @@
+import TheMovieDbSource from '../../data/themoviedb-source';
+import { createMovieItemTemplate } from '../templates/template-creator';
+
 const nowPlaying = {
   async render() {
     return `
-            <h2>Now Playing</h2>`;
+      <div class="content">
+        <h2 class="content__heading">Now Playing in Cinema</h2>
+        <div id="movies" class="movies">
+      </div>
+    `;
   },
 
   async afterRender() {
     // Fungsi ini akan dipanggil setelah render()
+    const movies = await TheMovieDbSource.nowPlayingMovies();
+    const moviesContainer = document.querySelector('#movies');
+    movies.forEach((movie) => {
+      moviesContainer.innerHTML += createMovieItemTemplate(movie);
+    });
   },
 };
 
