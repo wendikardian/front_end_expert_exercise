be0be2b44825378402da545ed6af2c771c700997
Wendi Kardian <69452468+wendikardian@users.noreply.github.com>
2023-05-22 15:20:05 +0700

    :

--- movie_catalogue/src/scripts/sw.js
+++ movie_catalogue/src/scripts/sw.js
@@ -1,16 +1,35 @@
+import CacheHelper from './utils/cache-helper';
+
+const assetsToCache = [
+  './',
+  './icons/maskable_icon.png',
+  './icons/maskable_icon_x48.png',
+  './icons/maskable_icon_x72.png',
+  './icons/maskable_icon_x96.png',
+  './icons/maskable_icon_x128.png',
+  './icons/maskable_icon_x192.png',
+  './icons/maskable_icon_x384.png',
+  './icons/maskable_icon_x512.png',
+  './index.html',
+  './favicon.png',
+  './app.bundle.js',
+  './app.webmanifest',
+  './sw.bundle.js',
+];
+
 /* eslint-disable no-unused-vars */
 self.addEventListener('install', (event) => {
   console.log('Installing Service Worker...');
-
+  event.waitUntill(CacheHelper.cachingAppShell([...assetsToCache]));
   // TODO : Caching App Shell Resource
 });
 
 self.addEventListener('activate', (event) => {
   console.log('Activating Service Worker...');
+  event.waitUntil(CacheHelper.deleteOldCache());
   // TODO : Delete Old Cache
 });
 
 self.addEventListener('fetch', (event) => {
-  console.log(event.request);
-  event.respondWith(fetch(event.request));
+  event.respondWith(CacheHelper.revalidateCache(event.request));
 });
